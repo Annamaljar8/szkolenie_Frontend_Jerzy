499804ca517e13958f167580b51c57304e780553
Oleksandra Kharytonova <oleksandra.kharytonova@execon.pl>
2021-10-21 11:58:41 +0200

    config library

--- warsztaty/vue.config.js
+++ warsztaty/vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  css: {
+    requireModuleExtension: false,
+    loaderOptions: {
+      scss: {
+        additionalData: `
+        @import "~vuetify/dist/vuetify.min.css";
+        `,
+      },
+    },
+    extract: false
+  },
+
 }
